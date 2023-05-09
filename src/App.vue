89222bf5ae1e57022a1efc3522cf3a754ed68f59
Pan-Pan-Pan <1425650574@qq.com>
2023-05-09 22:28:46 +0800

    TopNav, Login, Register

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import TopNav from './components/TopNav.vue';
-import ImageTable from './components/tables/ImageTable/ImageTable.vue';
 </script>
 
 <template>
-  <TopNav></TopNav>
-  <br>
-  <image-table></image-table>
+  
   <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     
@@ -21,9 +18,9 @@ import ImageTable from './components/tables/ImageTable/ImageTable.vue';
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
-  <RouterView /> -->
+  <RouterView />
 </template>
 
 <style scoped>
