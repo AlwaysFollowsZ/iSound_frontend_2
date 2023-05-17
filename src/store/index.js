ce2ca381af0fb48076b524592899e608ea84623b
Illli <254906270@qq.com>
2023-05-17 10:00:59 +0800

    fix css

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,14 @@ import Vuex from 'vuex'
 export default new Vuex.Store({
     //数据，相当于data
     state: {
-        colorMode: 'white',
+        colorMode: 'black',
         currentThemeColor: [200,200,200]
     },
     mutations: {
         changeColorMode(state) {
             state.colorMode = state.colorMode === 'white' ? 'black' : 'white'
         },
-        changeThemeColor(state,rgb) {
+        changeThemeColor(state, rgb) {
             state.currentThemeColor = rgb
         }
     }
