b38c15634e7f51e8cc2ae01aaa0669eb4325e7d0
Iris-Q <109052116+Iris-Q@users.noreply.github.com>
2023-05-17 16:18:20 +0800

    zjq0517
    未完成的播放页面

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -2,8 +2,8 @@
     import dayjs from 'dayjs';
     import relativeTime from 'dayjs/plugin/relativeTime';
     import { defineComponent } from 'vue';
-    import { NButton } from "naive-ui";
-    import { ChevronBack, StarOutline, FitnessOutline, WarningOutline, ChatbubbleEllipsesOutline } from '@vicons/ionicons5';
+    import { useMessage } from "naive-ui";
+    import { ChevronBack, StarOutline, FitnessOutline, WarningOutline, ChatbubbleEllipsesOutline, TrashOutline, CreateOutline } from '@vicons/ionicons5';
     dayjs.extend(relativeTime);
     export default defineComponent({
         name: 'PlayerView',
@@ -13,14 +13,47 @@
             FitnessOutline,
             WarningOutline,
             ChatbubbleEllipsesOutline,
+            TrashOutline,
+            CreateOutline,
         },
         setup() {
+            const message = useMessage();
             return {
+                handlePositiveClick() {
+                    message.info("该评论已删除");
+                },
+                handleNegativeClick() {
+                    message.info("取消");
+                },
                 dayjs,
+                value: ref(null),
             };
         },
         data() {
-            
+            return {
+                id: 0,
+                page: 1,
+                comments:[
+                    {
+                        id: 1,
+                        author: "Iris",
+                        avatar: '/src/assets/song4.jpg',
+                        content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.'
+                    },
+                    {
+                        id: 2,
+                        author: "zjq",
+                        avatar: "/src/assets/song6.jpg",
+                        content: "这是我的评论评论评论评论评论评论"
+                    },
+                    {
+                        id: 3,
+                        author: "中文试一下",
+                        avatar: "/src/assets/song1.jpg",
+                        content: "这是很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论很多行评论"
+                    },
+                ]
+            }
         },
         methods: {
             back() {
@@ -35,7 +68,19 @@
             complain() {
                 //todo
             },
-            comment() {
+            getImageUrl(url) {
+                return new URL(url, import.meta.url).href;
+            },
+            cleanComment() {
+                this.value=""
+            },
+            sendComment() {
+                //todo
+            },
+            deleteMyComment() {
+                //todo
+            },
+            editMyComment() {
                 //todo
             },
         }
@@ -112,26 +157,38 @@
                         <span style="margin-right: 3px; margin-top: 2px;">
                             <n-grid>
                                 <n-gi :span="23">
-                                    <span style="font-size: 24px;">
+                                    <span style="font-size: 22px;">
                                         全部评论
                                     </span>
                                 </n-gi>
                                 <n-gi :span="1">
-                                    <n-icon size="30"><ChatbubbleEllipsesOutline/></n-icon>
+                                    <n-icon size="27"><ChatbubbleEllipsesOutline/></n-icon>
                                 </n-gi>
                             </n-grid>
                         </span>
                     </template>
                         <div>
-                        <n-input
-                        style="margin-bottom: 30px;"
-                        placeholder="我的评论"
-                        type="textarea"
-                        :style="{'--n-border-radius' : `10px`}"
-                        :autosize="{
-                            minRows: 6,
-                            maxRows: 6
-                        }"/></div>
+                            <n-input
+                            style="margin-bottom: 15px;"
+                            maxlength="200" 
+                            show-count
+                            placeholder="我的评论"
+                            type="textarea"
+                            v-model:value="value"
+                            :style="{'--n-border-radius' : `10px`}"
+                            :autosize="{
+                                minRows: 6,
+                                maxRows: 6
+                            }"/>
+                            <div class="my-comment-button">
+                                <n-button class="clean-button" strong secondary type="tertiary" @click="cleanComment()">
+                                清空
+                                </n-button>
+                                <n-button calss="send-button" strong secondary type="tertiary" @click="sendComment()">
+                                发送
+                                </n-button>
+                            </div>
+                        </div>
                     </n-collapse-item>
                     </n-collapse>
                     
@@ -144,24 +201,53 @@
         <n-grid>
             <n-gi :span="4"></n-gi>
             <n-gi :span="16">
-                <div>
+                <div v-for="comment in comments">
                     <a-comment>
-                        <template #author><a style="font-size: 20px;">Han Solo</a></template>
+                        <template #actions>
+                            <span key="edit-comment">
+                                <span style="padding-left: 855px; cursor: auto">
+                                    <n-popover trigger="hover">
+                                        <template #trigger>
+                                        <n-icon size="18" @click="editMyComment()">
+                                        <CreateOutline/>
+                                        </n-icon>
+                                        </template>
+                                        <span>编辑我的评论</span>
+                                    </n-popover>
+                                </span>
+                            </span>
+                            <span key="delete-comment">
+                                <span style="padding-left: 3px; cursor: auto">
+                                    <n-popconfirm
+                                        @positive-click="handlePositiveClick"
+                                        @negative-click="handleNegativeClick"
+                                    >
+                                    <template #trigger>
+                                    <n-icon size="18" @click="deleteMyComment()">
+                                    <TrashOutline/>
+                                    </n-icon>
+                                    </template>
+                                    确认删除这条评论吗？
+                                    </n-popconfirm>
+                                </span>
+                            </span>
+                        </template>
+                        <template #author><a style="font-size: 18px;">{{ comment.author }}</a></template>
                         <template #avatar>
-                            <a-avatar src="src/assets/song5.png" alt="Han Solo" :size="50"/>
+                            <a-avatar src="/src/assets/song4.jpg" :size="50"/>
                         </template>
                         <template #content>
-                        <p style="font-size: 15px;">
-                            We supply a series of design principles, practical patterns and high quality design
-                            resources (Sketch and Axure), to help people create their product prototypes beautifully and
-                            efficiently.
+                        <p style="font-size: 13.5px; margin-top: 8px; margin-bottom: 0px;">
+                            {{ comment.content }}
                         </p>
                         </template>
                         <template #datetime>
                         <a-tooltip :title="dayjs().format('YYYY-MM-DD HH:mm:ss')">
-                            <span style="margin-bottom: 0;">{{ dayjs().fromNow() }}</span>
+                            <span style="margin-bottom: 0; font-size: 10px;">{{ dayjs().fromNow() }}</span>
+                            
                         </a-tooltip>
                         </template>
+                        
                     </a-comment>
                 </div> 
             </n-gi>
@@ -169,6 +255,7 @@
         </n-grid>
     </div>
 </template>
+
 <style scoped>
 .back-button {
     width: 40px;
@@ -223,8 +310,19 @@
 }
 :deep(.ant-comment-inner){
     padding-top: 0;
+    padding-bottom: 5px;
+}
+:deep(.ant-comment-actions) {
+    margin-top: 0;
 }
 .edit-comment {
     margin-top: 24px;
+}
+.clean-button {
+    margin-right: 15px;
+    margin-left: 849px;
+}
+.my-comment-button {
+    margin-bottom: 30px;
 }
 </style>
