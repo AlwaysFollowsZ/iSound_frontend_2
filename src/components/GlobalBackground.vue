e1d051371a77565c796485eec076f04155f85ad5
Illli <254906270@qq.com>
2023-05-23 16:16:15 +0800

    fix

--- src/components/GlobalBackground.vue
+++ src/components/GlobalBackground.vue
@@ -1,6 +1,6 @@
 <script>
 import { ThumbDislike16Filled } from '@vicons/fluent';
-import { backgroundColor, getRGBString, findSimilarColors,globalThemeColor } from '/src/colorMode';
+import { backgroundColor, getRGBString, findSimilarColors, globalThemeColor } from '/src/colorMode';
 export default {
     data() {
         watch(backgroundColor, () => {//切换背景色优先
@@ -27,30 +27,42 @@ export default {
                 let updatedColor
                 let transitionDelta
                 do {
-                    updatedColor = findSimilarColors(color, 100, 0.2)[Math.floor(Math.random() * 100)]
+                    updatedColor = findSimilarColors(color, 100,this.Similarity)[Math.floor(Math.random() * 100)]
                     //计算几何变化值
                     transitionDelta = Math.sqrt(Math.pow((beforeColor[0] - updatedColor[0]), 2) + Math.pow((beforeColor[1] - updatedColor[1]), 2) + Math.pow((beforeColor[2] - updatedColor[2]), 2))
-                } while (transitionDelta < 10)//随机选取一个相近的颜色,数值越大相似度越高
-                //如果颜色变化过小则重新寻找
+                } while (transitionDelta < 10)//随机选取一个相近的颜色,数值越大相似度越高,如果颜色变化过小则重新寻找
                 //更新过渡时间和当前颜色
                 this.currentColor = updatedColor
                 this.transitionTime = transitionDelta / this.updateSpeed
+                //约束过渡时间下限
                 if (this.transitionTime < 1) {
-                    this.transitionTime=1
-                }//过渡时间下限约束
-                console.log(this.transitionTime, updatedColor);
+                    this.transitionTime = 1
+                }
                 setTimeout(loopAnimation, this.transitionTime * 1000)
             }
-            setTimeout(loopAnimation,1000)
+            if (this.animateSwitch === true) {
+                setTimeout(loopAnimation, 1000)
+            }
         }
     },
     props: {
         updateSpeed: {//背景动画的变化快慢
             type: Number,
-            default:15,
+            default: 15,
             validator(value) {
                 return value > 0
             }
+        },
+        Similarity: {//变换颜色的相似度
+            type: Number,
+            default: 0.2,
+            validator(value) {
+                return 0 <= value <= 1
+            }
+        },
+        animateSwitch: {//是否开启变换颜色
+            type: Boolean,
+            default: true
         }
     }
 }
@@ -71,7 +83,4 @@ export default {
     height: 100vh;
     width: 100vw;
 }
-
-
-
 </style>
