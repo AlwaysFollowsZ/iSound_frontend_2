deb4cd8f1ec3ad75dfa14c884e734d2917ddf68b
Pan-Pan-Pan <1425650574@qq.com>
2023-05-27 13:46:12 +0800

    注册页面

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,31 +1,115 @@
 <template>
     <!-- <n-button @click="showLogin = true">登录</n-button> -->
-    <n-modal :show="showRegister" class="round-corner">
-        <div>
-            <n-card :style="{ 'width': `600px`, '--n-border-radius': `20px`, '--n-padding-top': '30px' }" :bordered="false" role="dialog"
-                aria-modal="true">
-                <template #header>
-                    <div>
-                        <n-grid :x-gap="0">
-                            <n-gi :span="2"></n-gi>
-                            <n-gi :span="20">
-                                <div class="register-card-title">加入我们：一起建设iSound</div>
-                            </n-gi>
-                            <n-gi :span="2">
-                                <div style="padding-top: 30%" @click="closeRWindow">
-                                    <close-outline />
-                                </div>
-                            </n-gi>
-                        </n-grid>
-                    </div>
-                </template>
+    <n-modal :show="showRegister" class="round-corner" :style="{'background-color': this.colorMode === 'white' ? 'white' : 'rgb(50,50,50)'}">
+        <div class="outer-container">
+            <div class="title-container">
+                <div style="margin-bottom: 30px;">
+                    <n-grid :x-gap="0">
+                        <n-gi :span="2"></n-gi>
+                        <n-gi :span="20">
+                            <div class="register-card-title" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">加入我们：一起建设iSound</div>
+                        </n-gi>
+                        <n-gi :span="2">
+                            <div class="close-icon" style="padding-top: 5px" @click="closeRWindow" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+                                <n-icon size="40"><close-outline/></n-icon>
+                            </div>
+                        </n-gi>
+                    </n-grid>
+                </div>
+            </div>
+            <div class="body-container">
                 <n-grid x-gap="0">
-                    <n-gi :span="10">
+                    <n-gi :span="9">
                         <img class="login-image"
                             src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
                     </n-gi>
-                    <n-gi :span="14">
-                        <span style="font-size: 18px; font-weight: 500">用户名 </span>
+                    <n-gi :span="15">
+                        <div class="body-item">
+                            <n-grid :x-gap="12">
+                                <n-gi :span="6">
+                                    <div class="body-item-prompt" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+                                        用户名
+                                    </div>
+                                </n-gi>
+                                <n-gi :span="16">
+                                    <n-input type="text" placeholder="请输入用户名" :value="username" @input="username = $event" clearable 
+                                        :style="{
+                                            '--n-color': this.colorMode === 'white' ? 'white' : 'rgb(72,72,72)',
+                                            '--n-color-focus': this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
+                                            '--n-text-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-caret-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-border-hover': 'transparent',
+                                            '--n-border-focus': 'transparent',
+                                            '--n-placeholder-color': this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
+                                            '--n-border-radius': '8px',
+                                        }"
+                                    />
+                                </n-gi>
+                            </n-grid>
+                        </div>
+                        <div class="body-item" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+                            <n-grid :x-gap="12">
+                                <n-gi :span="6">
+                                    <div class="body-item-prompt">
+                                        密码
+                                    </div>
+                                </n-gi>
+                                <n-gi :span="16">
+                                    <n-input v-model="password" type="password" show-password-on="mousedown" placeholder="请输入密码" :minlength="8" :value="password1" @input="password1 = $event"
+                                        :style="{
+                                            '--n-color': this.colorMode === 'white' ? 'white' : 'rgb(72,72,72)',
+                                            '--n-color-focus': this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
+                                            '--n-text-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-caret-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-border-hover': 'transparent',
+                                            '--n-border-focus': 'transparent',
+                                            '--n-placeholder-color': this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
+                                            '--n-border-radius': '8px',
+                                        }"
+                                    />
+                                </n-gi>
+                            </n-grid>
+                        </div>
+                        <div class="body-item">
+                            <n-grid :x-gap="12">
+                                <n-gi :span="6">
+                                    <div class="body-item-prompt" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+                                        确认密码
+                                    </div>
+                                </n-gi>
+                                <n-gi :span="16">
+                                    <n-input v-model="password" type="password" show-password-on="mousedown" placeholder="请再次输入密码" :minlength="8" :value="password2" @input="password2 = $event"
+                                        :style="{
+                                            '--n-color': this.colorMode === 'white' ? 'white' : 'rgb(72,72,72)',
+                                            '--n-color-focus': this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
+                                            '--n-text-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-caret-color': this.colorMode === 'white' ? 'black' : 'white',
+                                            '--n-border-hover': 'transparent',
+                                            '--n-border-focus': 'transparent',
+                                            '--n-placeholder-color': this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
+                                            '--n-border-radius': '8px',
+                                        }"
+                                    />
+                                </n-gi>
+                            </n-grid>
+                        </div>
+                        <div class="">
+                            <n-grid class="login-button-top" x-gap="6">
+                                <n-gi :span="7"></n-gi>
+                                <n-gi :span="7">
+                                    <n-button strong secondary type="success" @click="register" :style="{'--n-border-radius': '6px'}">
+                                        注册
+                                    </n-button>
+                                </n-gi>
+                                <n-gi :span="7">
+                                    <n-button strong secondary type="Warning" @click="closeRWindow" :style="{'--n-border-radius': '6px'}">
+                                        取消
+                                    </n-button>
+                                </n-gi>
+                                <n-gi :span="3"></n-gi>
+                            </n-grid>
+                        </div>
+                        <!-- <span style="font-size: 18px; font-weight: 500">用户名 </span>
                         <n-input type="text" placeholder="请输入用户名" :value="username" @input="username = $event" clearable />
                         <span style="font-size: 18px; font-weight: 500">密码</span>
                         <n-input v-model="password" type="password" show-password-on="mousedown" placeholder="请输入密码" :minlength="8" :value="password1" @input="password1 = $event"/>
@@ -42,23 +126,27 @@
                                     取消
                                 </n-button>
                             </n-gi>
-                        </n-grid>
+                        </n-grid> -->
                     </n-gi>
                 </n-grid>
-                <div class="login-notice-text">
-                    注册账号并登录表示用户承诺遵守网站公约，同时网站承诺将保护用户隐私信息。
+            </div>
+                <div class="login-notice-text" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+                    注册账号表示您将承诺遵守网站公约，网站承诺保护您的隐私信息。
                 </div>
-            </n-card>
         </div>
     </n-modal>
 </template>
 
 <script>
 import { CloseOutline } from '@vicons/ionicons5'
+import { mapState } from 'vuex'
 export default {
     name: "RegisterView",
     components: {
         CloseOutline,
+    },
+    computed: {
+        ...mapState(['accentColor', 'colorMode']),
     },
     props: {
         showRegister: Boolean,
@@ -95,6 +183,30 @@ export default {
 </script>
 
 <style scoped>
+.close-icon:hover {
+    cursor: pointer;
+}
+.outer-container {
+    max-width: 640px; 
+    border-radius: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 25px;
+    padding-right: 25px;
+}
+.title-container {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+.body-item {
+    margin-bottom: 25px;
+}
+.body-item-prompt {
+    padding-top: 3px;
+    text-align: right;
+    font-size: 16px;
+    font-weight: 400;
+}
 .register-card-title {
     display: flex;
     justify-content: center;
