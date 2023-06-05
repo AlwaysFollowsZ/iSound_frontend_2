66e8cef245f83c78c9cf6a616f6ba1f896d3d6e0
KaQyn <3140573900@qq.com>
2023-06-05 10:20:53 +0800

    1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,42 @@
 <template>
   <!-- <n-button @click="showLogin = true">登录</n-button> -->
-  <n-modal :show="showLogin" :style="{'background-color': this.colorMode === 'white' ? 'white' : 'rgb(50,50,50)'}" :block-scroll="false">
+  <n-modal
+    :show="showLogin"
+    :style="{
+      'background-color': this.colorMode === 'white' ? 'white' : 'rgb(50,50,50)',
+    }"
+    :block-scroll="false"
+  >
     <div class="outer-container">
       <div class="title-container">
         <div style="margin-bottom: 30px">
           <n-grid>
             <n-gi :span="2"></n-gi>
             <n-gi :span="20">
-              <div class="login-card-title" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">开启独一无二的音乐之旅</div>
-              <div class="login-card-title" :style="{'color': this.colorMode === 'white' ? 'black' : 'white', 'font-size': '25px'}">登录你的 iSound 账号</div>
+              <div
+                class="login-card-title"
+                :style="{ color: this.colorMode === 'white' ? 'black' : 'white' }"
+              >
+                开启独一无二的音乐之旅
+              </div>
+              <div
+                class="login-card-title"
+                :style="{
+                  color: this.colorMode === 'white' ? 'black' : 'white',
+                  'font-size': '25px',
+                }"
+              >
+                登录你的 iSound 账号
+              </div>
             </n-gi>
             <n-gi :span="2">
-              <div class="close-icon" style="padding-top: 5px" @click="closeLWindow" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
-                <n-icon size="40"><close-outline/></n-icon>
+              <div
+                class="close-icon"
+                style="padding-top: 5px"
+                @click="closeLWindow"
+                :style="{ color: this.colorMode === 'white' ? 'black' : 'white' }"
+              >
+                <n-icon size="40"><close-outline /></n-icon>
               </div>
             </n-gi>
           </n-grid>
@@ -23,82 +47,124 @@
           <n-grid>
             <n-gi :span="3"></n-gi>
             <n-gi :span="18">
-              <n-input type="text" placeholder="用户名" :value="username" @input="username = $event" 
+              <n-input
+                type="text"
+                placeholder="用户名"
+                :value="username"
+                @input="username = $event"
                 :style="{
                   '--n-color': this.colorMode === 'white' ? 'white' : 'rgb(72,72,72)',
-                  '--n-color-focus': this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
+                  '--n-color-focus':
+                    this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
                   '--n-text-color': this.colorMode === 'white' ? 'black' : 'white',
                   '--n-caret-color': this.colorMode === 'white' ? 'black' : 'white',
                   '--n-border-hover': 'transparent',
                   '--n-border-focus': 'transparent',
-                  '--n-placeholder-color': this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
+                  '--n-placeholder-color':
+                    this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
                   '--n-border-radius': '5px',
                   '--n-height': '55px',
                   '--n-font-size': '18px',
                   '--n-border': '1px solid ' + 'rgba(' + this.accentColor + ', 0.8)',
-                  '--n-box-shadow-focus': '0 0 0 2px ' + 'rgba(' + this.accentColor + ', 0.6)',
+                  '--n-box-shadow-focus':
+                    '0 0 0 2px ' + 'rgba(' + this.accentColor + ', 0.6)',
                 }"
                 @keyup.enter="this.passwdInputShouldShow = true"
               >
                 <template #suffix>
-                  <n-icon 
-                    class="username-forward-icon" 
-                    size="30" 
+                  <n-icon
+                    class="username-forward-icon"
+                    size="30"
                     @click="this.passwdInputShouldShow = true"
-                    :class="{'animate__animated animate__zoomOut': this.passwdInputShouldShow}"
-                  ><ArrowForwardCircleOutline/></n-icon>
+                    :class="{
+                      'animate__animated animate__zoomOut': this.passwdInputShouldShow,
+                    }"
+                    ><ArrowForwardCircleOutline
+                  /></n-icon>
                 </template>
               </n-input>
             </n-gi>
             <n-gi :span="3"></n-gi>
           </n-grid>
         </div>
-        <div class="body-item animate__animated animate__fadeInDown" v-if="passwdInputShouldShow" >
+        <div
+          class="body-item animate__animated animate__fadeInDown"
+          v-if="passwdInputShouldShow"
+        >
           <n-grid>
             <n-gi :span="3"></n-gi>
             <n-gi :span="18">
-              <n-input type="password"  placeholder="密码" :minlength="8" :value="password" @input="password = $event"
+              <n-input
+                type="password"
+                placeholder="密码"
+                :minlength="8"
+                :value="password"
+                @input="password = $event"
                 :style="{
                   '--n-color': this.colorMode === 'white' ? 'white' : 'rgb(72,72,72)',
-                  '--n-color-focus': this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
+                  '--n-color-focus':
+                    this.colorMode === 'white' ? 'white' : 'rgb(100,100,100)',
                   '--n-text-color': this.colorMode === 'white' ? 'black' : 'white',
                   '--n-caret-color': this.colorMode === 'white' ? 'black' : 'white',
                   '--n-border-hover': 'transparent',
                   '--n-border-focus': 'transparent',
-                  '--n-placeholder-color': this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
+                  '--n-placeholder-color':
+                    this.colorMode === 'white' ? 'grey' : 'rgb(200,200,200)',
                   '--n-border-radius': '8px',
                   '--n-height': '55px',
                   '--n-font-size': '18px',
                   '--n-border': '1px solid ' + 'rgba(' + this.accentColor + ', 0.8)',
-                  '--n-box-shadow-focus': '0 0 0 2px ' + 'rgba(' + this.accentColor + ', 0.6)',
-                  '--n-icon-size': '25px'
+                  '--n-box-shadow-focus':
+                    '0 0 0 2px ' + 'rgba(' + this.accentColor + ', 0.6)',
+                  '--n-icon-size': '25px',
                 }"
                 @keyup.enter="login"
               >
                 <template #suffix>
-                  <n-icon 
-                    class="username-forward-icon animate__animated animate__zoomIn" 
-                    size="30" 
+                  <n-icon
+                    class="username-forward-icon animate__animated animate__zoomIn"
+                    size="30"
                     @click="login"
                     style="animation-delay: 1s"
-                    :color="(this.accentColor === '0,0,0' || this.accentColor === '255,255,255') ? 'grey' : 'rgb(' + this.accentColor + ')'"
-                  ><ArrowForwardCircleOutline/></n-icon>
+                    :color="
+                      this.accentColor === '0,0,0' || this.accentColor === '255,255,255'
+                        ? 'grey'
+                        : 'rgb(' + this.accentColor + ')'
+                    "
+                    ><ArrowForwardCircleOutline
+                  /></n-icon>
                 </template>
               </n-input>
             </n-gi>
             <n-gi :span="3"></n-gi>
           </n-grid>
-        </div><div style="height: 75px" v-else></div>
-        <div class="login-prompt" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
+        </div>
+        <div style="height: 75px" v-else></div>
+        <div
+          class="login-prompt"
+          :style="{ color: this.colorMode === 'white' ? 'black' : 'white' }"
+        >
           登录 iSound 账号，体验评论、上传、收藏、分享新功能。
         </div>
-        <div class="login-sub-prompt" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
-          未注册账号？<span class="login-link" @click="switchToRegister">前往创建你的 iSound 账号
-          <div style="display: inline-block; vertical-align: top"><n-icon size="20px"><OpenOutline/></n-icon></div></span>
+        <div
+          class="login-sub-prompt"
+          :style="{ color: this.colorMode === 'white' ? 'black' : 'white' }"
+        >
+          未注册账号？<span class="login-link" @click="switchToRegister"
+            >前往创建你的 iSound 账号
+            <div style="display: inline-block; vertical-align: top">
+              <n-icon size="20px"><OpenOutline /></n-icon></div
+          ></span>
         </div>
-        <div class="login-sub-prompt" :style="{'color': this.colorMode === 'white' ? 'black' : 'white'}">
-          忘记密码？<span class="login-link" @click="switch2ResetPasswd">前往重置密码
-          <div style="display: inline-block; vertical-align: top"><n-icon size="20px"><OpenOutline/></n-icon></div></span>
+        <div
+          class="login-sub-prompt"
+          :style="{ color: this.colorMode === 'white' ? 'black' : 'white' }"
+        >
+          忘记密码？<span class="login-link" @click="switch2ResetPasswd"
+            >前往重置密码
+            <div style="display: inline-block; vertical-align: top">
+              <n-icon size="20px"><OpenOutline /></n-icon></div
+          ></span>
         </div>
       </div>
     </div>
@@ -106,9 +172,9 @@
 </template>
 
 <script>
-import { CloseOutline, ArrowForwardCircleOutline, OpenOutline } from "@vicons/ionicons5"
-import { mapState } from 'vuex'
-import 'animate.css'
+import { CloseOutline, ArrowForwardCircleOutline, OpenOutline } from "@vicons/ionicons5";
+import { mapState } from "vuex";
+import "animate.css";
 export default {
   name: "LoginView",
   data() {
@@ -119,7 +185,7 @@ export default {
     };
   },
   computed: {
-    ...mapState(['accentColor', 'colorMode']),
+    ...mapState(["accentColor", "colorMode"]),
   },
   components: {
     CloseOutline,
@@ -150,6 +216,8 @@ export default {
         if (response.data.code == "0") {
           this.$cookies.set("userid", response.data.up.id);
           this.$cookies.set("is_superuser", response.data.up.is_superuser);
+          if (response.data.up.is_superuser) {
+          }
           this.changeToLoggedIn();
           this.closeLWindow();
         } else if (response.data.code == "-1") {
@@ -167,7 +235,7 @@ export default {
   cursor: pointer;
 }
 .outer-container {
-  max-width: 640px; 
+  max-width: 640px;
   height: 470px;
   border-radius: 20px;
   padding-top: 20px;
